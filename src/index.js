2fe378ec8fd4fbd07799014a2368f9239e701863
sungyotkim <sungyotkim@gmail.com>
2022-09-16 16:56:12 -0400

    fix photo render issue

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 document.addEventListener("DOMContentLoaded", e => {
   const btn = document.querySelector("#btn")
-  const petsUrl = '../dist/data/shelterInfo.json'
+  const petsUrl = '../dist/data/petInfo.json'
+  const container = document.querySelector('.container-center')
 
 
-  console.log("Test!")
-
-  btn.addEventListener('click', reqData);
+  // btn.addEventListener('click', reqData);
   
-  function reqData() {
-    fetch(petsUrl)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-  }
+  // function reqData() {
+  //   fetch(petsUrl)
+  //     .then(res => res.json())
+  //     .then(data => adder(data))
+  //     .catch((error) => {
+  //       console.error(error);
+  //     })
+  // }
+
+  // function adder(data) {
+  //   console.log(data);
+  //   console.log(petsUrl)
+  //   const ul = document.createElement('ul');
+  //   container.append(ul);
+  //   data.forEach(ele => {
+  //     const li = document.createElement('li');
+  //     li.textContent = `${ele.name}`;
+  //     const pic = document.createElement('img');
+  //     pic.src = `../dist/data/pets/${ele.name.toLowerCase()}/photos/${ele.photos[0]}`
+  //     li.appendChild(pic)
+  //     ul.append(li);
+  //   })
+  // }
 });
