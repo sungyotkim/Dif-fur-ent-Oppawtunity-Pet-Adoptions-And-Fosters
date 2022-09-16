39efa2a422f8154e85b4ee06321918d16af3d405
sungyotkim <sungyotkim@gmail.com>
2022-09-16 16:39:26 -0400

    fix fetch functinoality to local json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", e => {
+  const btn = document.querySelector("#btn")
+  const petsUrl = '../dist/data/shelterInfo.json'
+
+
   console.log("Test!")
 
-
+  btn.addEventListener('click', reqData);
+  
+  function reqData() {
+    fetch(petsUrl)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
 });
