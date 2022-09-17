d4390db9f1a63f37e7e71c9b82cd9064b356c32b
sungyotkim <sungyotkim@gmail.com>
2022-09-17 17:44:37 -0400

    add interactiong between species and breed dropdown

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,8 +1,8 @@
 const selectBtns = document.querySelectorAll(".dropdown-top")
-const items = document.querySelectorAll(".item");
 const speciesItems = document.querySelectorAll(".item-species");
 const breedItems = document.querySelectorAll(".item-breed");
 const ageItems = document.querySelectorAll(".item-age");
+const breedItemsList = document.querySelector(".breed-items-list");
 
 //toggles for all dropdowns
 selectBtns.forEach(selectBtn => {
@@ -19,11 +19,44 @@ speciesItems.forEach(speciesItem => {
         let btnTexts = document.querySelectorAll(".dropdown-btn-text");
     
         if(speciesChecked && speciesChecked.length === 1) {
-            btnTexts[0].innerText = `${speciesChecked[0].innerText}`
+            btnTexts[0].innerText = `${speciesChecked[0].innerText}`;
+            let hideList = speciesChecked[0].innerText === 'Canine' ?
+            document.querySelectorAll('.Feline') :
+            document.querySelectorAll('.Canine');
+            hideList.forEach(li => {
+                if (li.classList.contains("checked")) {
+                    li.classList.toggle("checked");
+                    let breedChecked = document.querySelectorAll(".item-breed.checked");
+                    if(breedChecked && breedChecked.length === 1) {
+                        btnTexts[1].innerText = `${breedChecked[0].innerText}`;
+                    } else if (breedChecked && breedChecked.length > 1) {
+                        btnTexts[1].innerText = "Multiple"
+                    } else {
+                        btnTexts[1].innerText = "Any"
+                    }
+                }
+                li.setAttribute("style", "display: none")
+            })
         } else if (speciesChecked && speciesChecked.length > 1) {
-            btnTexts[0].innerText = "Multiple"
+            btnTexts[0].innerText = "Multiple";
+            let showList1 = document.querySelectorAll(`.Canine`);
+            let showList2 = document.querySelectorAll(`.Feline`);
+            showList1.forEach(li => {
+                li.removeAttribute("style")
+            })
+            showList2.forEach(li => {
+                li.removeAttribute("style")
+            })
         } else {
-            btnTexts[0].innerText = "Any"
+            btnTexts[0].innerText = "Any";
+            let showList1 = document.querySelectorAll(`.Canine`);
+            let showList2 = document.querySelectorAll(`.Feline`);
+            showList1.forEach(li => {
+                li.removeAttribute("style")
+            })
+            showList2.forEach(li => {
+                li.removeAttribute("style")
+            })
         }
     })
 })
@@ -36,7 +69,15 @@ breedItems.forEach(breedItem => {
         let btnTexts = document.querySelectorAll(".dropdown-btn-text");
 
         if(breedChecked && breedChecked.length === 1) {
-            btnTexts[1].innerText = `${breedChecked[0].innerText}`
+            let felines = ['American Short Hair', 'Domestic Medium Hair', 'Domestic Short Hair', 'Exotic Short Hair']
+            if (felines.includes(breedChecked[0].innerText)) {
+                let letters = breedChecked[0].innerText.split(' ').map(word => {
+                    return word[0]
+                }).join("")
+                btnTexts[1].innerText = `${letters}`;
+            } else {
+                btnTexts[1].innerText = `${breedChecked[0].innerText}`;
+            }
         } else if (breedChecked && breedChecked.length > 1) {
             btnTexts[1].innerText = "Multiple"
         } else {
@@ -53,7 +94,7 @@ ageItems.forEach(ageItem => {
         let btnTexts = document.querySelectorAll(".dropdown-btn-text");
 
         if(ageChecked && ageChecked.length === 1) {
-            btnTexts[2].innerText = `${ageChecked[0].innerText}`
+            btnTexts[2].innerText = `${ageChecked[0].innerText.split(' ')[0]}`
         } else if (ageChecked && ageChecked.length > 1) {
             btnTexts[2].innerText = "Multiple"
         } else {
