632b4dd392d958dd09c96b3c5719f8ba82e03e6b
sungyotkim <sungyotkim@gmail.com>
2022-09-20 14:00:27 -0400

    set up photo gallery

---
+++ src/scripts/photo-gallery.js
@@ -0,0 +1,34 @@
+const petsUrl = '../../dist/data/petInfo.json';
+const mainContainer = document.querySelector('.main-photo-container');
+const subContainer = document.querySelector('.sub-photo-container');
+
+function reqPetInfo(profilePet) {
+  fetch(petsUrl)
+        .then(res => res.json())
+        .then(data => {
+          data.forEach(pet => {
+            if (pet.name === profilePet) {
+              populateMainPhoto(pet);
+              populateSubPhotos(pet);
+            }
+          })
+        })
+        .catch(error => {
+            console.log(error)
+        }) 
+}
+
+reqPetInfo('Nara');
+
+function populateMainPhoto(pet, num = 0) {
+  const photo = document.createElement('img');
+  photo.src = `../../dist/data/pets/${pet.name.toLowerCase()}/photos/${pet.photos[num]}`;
+  mainContainer.appendChild(photo);
+}
+
+function populateSubPhotos(pet) {
+  
+  const card = document.createElement('div');
+  card.setAttribute('class', 'photo-card');
+  const photo = document.createElement('img');
+}
