f96a7315c3f810c356cbe37694b8041c874de5c6
sungyotkim <sungyotkim@gmail.com>
2022-09-20 15:09:43 -0400

    add sub video container

--- src/scripts/video-gallery.js
+++ src/scripts/video-gallery.js
@@ -25,11 +25,11 @@ reqPetInfo('Nara');
 function populateMainVideo(pet, num = 0) {
   mainContainer.innerHTML = '';
 
-  const video = document.createElement('video');
-  video.setAttribute('autoplay', 'autoplay');
-  video.setAttribute('controls', 'controls');
-  video.src = `../../dist/data/pets/${pet.name.toLowerCase()}/videos/${pet.videos[num]}`
-  mainContainer.appendChild(video);
+  const vid = document.createElement('video');
+  vid.setAttribute('autoplay', 'autoplay');
+  vid.setAttribute('controls', 'controls');
+  vid.src = `../../dist/data/pets/${pet.name.toLowerCase()}/videos/${pet.videos[num]}`
+  mainContainer.appendChild(vid);
 }
 
 function populateReviews(pet, num = 0) {
@@ -42,5 +42,20 @@ function populateReviews(pet, num = 0) {
 }
 
 function populateSubVideos(pet) {
+  pet.videos.forEach(video => {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'video-card');
+    const vid = document.createElement('video');
+    vid.setAttribute('controls', 'false');
+    vid.src = `../../dist/data/pets/${pet.name.toLowerCase()}/videos/${video}`;
+    card.appendChild(vid);
+    subContainer.appendChild(card);
+    console.log(card)
 
+    card.addEventListener("click", () => {
+      let index = pet.videos.indexOf(video);
+      populateMainVideo(pet, index);
+      populateReviews(pet, index);
+    })
+  })
 }
