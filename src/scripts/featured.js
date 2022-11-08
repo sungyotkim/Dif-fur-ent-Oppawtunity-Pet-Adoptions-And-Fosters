a1bac8a188dc67c7761ab681ba3dd6c6b4803329
sungyotkim <sungyotkim@gmail.com>
2022-11-08 10:18:17 -0500

    change countdown date

--- src/scripts/featured.js
+++ src/scripts/featured.js
@@ -1,117 +1,127 @@
-const container = document.querySelector('.featured-inner-container');
+const container = document.querySelector(".featured-inner-container");
 const featuredContainer = document.querySelector(".featured-container");
-const petsUrl = './dist/data/petInfo.json';
-const profile = require('./profile');
-
+const petsUrl = "./dist/data/petInfo.json";
+const profile = require("./profile");
 
 function reqPetPhoto() {
-    fetch(petsUrl)
-        .then(res => res.json())
-        .then(data => photoAdder(data))
-        .catch(error => {
-            console.log(error)
-        }) 
+  fetch(petsUrl)
+    .then((res) => res.json())
+    .then((data) => photoAdder(data))
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function photoAdder(data) {
-    let petsWithVideos = data.filter(pet => pet.videos.length > 0 && pet.photos.length > 1);
-    let featuredPets = [];
-    while (featuredPets.length < 5) {
-        let sampleIdx = Math.floor(Math.random() * petsWithVideos.length);
-        let sample = petsWithVideos[sampleIdx];
-        featuredPets.push(sample);
-        petsWithVideos.splice(sampleIdx, 1)
-    }
+  let petsWithVideos = data.filter(
+    (pet) => pet.videos.length > 0 && pet.photos.length > 1
+  );
+  let featuredPets = [];
+  while (featuredPets.length < 5) {
+    let sampleIdx = Math.floor(Math.random() * petsWithVideos.length);
+    let sample = petsWithVideos[sampleIdx];
+    featuredPets.push(sample);
+    petsWithVideos.splice(sampleIdx, 1);
+  }
 
-    featuredPets.forEach(currentPet => {
-        const card = document.createElement('a');
-        card.setAttribute('class', 'featured-card');
-        const img = document.createElement('img');
-        img.src = `./dist/data/pets/${currentPet.name.toLowerCase()}/photos/${currentPet.photos[1]}`;
-        img.setAttribute('width', '50px')
-        img.setAttribute('class', 'featured-img')
-        const cardTitle = document.createElement('div');
-        cardTitle.innerText = `${currentPet.name} the ${currentPet.characteristics[0]} ${currentPet.species === 'Canine' ? 'dog' : 'cat'}`
-        cardTitle.setAttribute('class', 'featured-card-title');
-        const cardName = document.createElement('div');
-        const cardDescription = document.createElement('div');
-        cardName.innerText = `${currentPet.name} the ${currentPet.characteristics[0]} ${currentPet.species === 'Canine' ? 'dog' : 'cat'}`
-        cardName.setAttribute('class', 'featured-card-name');
-        cardDescription.innerText = `${currentPet.bio} Click to see more!`;
-        cardDescription.setAttribute('class', 'featured-card-bio');
-        card.appendChild(img);
-        card.appendChild(cardTitle);
-        card.appendChild(cardName);
-        card.appendChild(cardDescription);
-        container.append(card)
+  featuredPets.forEach((currentPet) => {
+    const card = document.createElement("a");
+    card.setAttribute("class", "featured-card");
+    const img = document.createElement("img");
+    img.src = `./dist/data/pets/${currentPet.name.toLowerCase()}/photos/${
+      currentPet.photos[1]
+    }`;
+    img.setAttribute("width", "50px");
+    img.setAttribute("class", "featured-img");
+    const cardTitle = document.createElement("div");
+    cardTitle.innerText = `${currentPet.name} the ${
+      currentPet.characteristics[0]
+    } ${currentPet.species === "Canine" ? "dog" : "cat"}`;
+    cardTitle.setAttribute("class", "featured-card-title");
+    const cardName = document.createElement("div");
+    const cardDescription = document.createElement("div");
+    cardName.innerText = `${currentPet.name} the ${
+      currentPet.characteristics[0]
+    } ${currentPet.species === "Canine" ? "dog" : "cat"}`;
+    cardName.setAttribute("class", "featured-card-name");
+    cardDescription.innerText = `${currentPet.bio} Click to see more!`;
+    cardDescription.setAttribute("class", "featured-card-bio");
+    card.appendChild(img);
+    card.appendChild(cardTitle);
+    card.appendChild(cardName);
+    card.appendChild(cardDescription);
+    container.append(card);
 
-        card.addEventListener("click", () => {
-            featuredContainer.classList.toggle('hidden');
-            profile.renderProfile(currentPet);
-        })
-    })
+    card.addEventListener("click", () => {
+      featuredContainer.classList.toggle("hidden");
+      profile.renderProfile(currentPet);
+    });
+  });
 }
 
-reqPetPhoto()
+reqPetPhoto();
 
 function timer() {
-    let countDownDate = new Date("Oct 1, 2022 00:00:00").getTime();
+  let countDownDate = new Date("June 10, 2023 00:00:00").getTime();
 
-    let x = setInterval(function() {
-        let timeNow = new Date().getTime();
+  let x = setInterval(function () {
+    let timeNow = new Date().getTime();
 
-        let timeDistance = countDownDate - timeNow;
+    let timeDistance = countDownDate - timeNow;
 
-        let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
-        let hours = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-        let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+    let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(
+      (timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
 
-        let goalTimer = document.getElementById("goal-timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    let goalTimer = (document.getElementById("goal-timer").innerHTML =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ");
 
-        if (timeDistance < 0 ) {
-            clearInterval(x);
-            goalTimer.innerHTML = "Time's up!"
-        }
-    }, 1000);
+    if (timeDistance < 0) {
+      clearInterval(x);
+      goalTimer.innerHTML = "Time's up!";
+    }
+  }, 1000);
 }
 
 timer();
 
 //fake incrementer to simulate adoptions
 function goalIncrementer() {
-    let goal = document.getElementById("adopted-numbers");
-    let max = document.getElementById("goal-numbers").innerText;
+  let goal = document.getElementById("adopted-numbers");
+  let max = document.getElementById("goal-numbers").innerText;
 
-    if (parseFloat(goal.innerText) < parseFloat(max)) {   
-        let incrementer = setInterval(() => {
-            let current = goal.innerText;
-            current = current.split(",").join("");
-            let next = parseInt(current) + 1;
-            goal.innerText = next.toLocaleString("en-US");
-            goalProgress();
-            if (goal.innerText === max) {
-                clearInterval(incrementer);
-            }
-        }, 5000);
-    }
+  if (parseFloat(goal.innerText) < parseFloat(max)) {
+    let incrementer = setInterval(() => {
+      let current = goal.innerText;
+      current = current.split(",").join("");
+      let next = parseInt(current) + 1;
+      goal.innerText = next.toLocaleString("en-US");
+      goalProgress();
+      if (goal.innerText === max) {
+        clearInterval(incrementer);
+      }
+    }, 5000);
+  }
 }
 
 goalIncrementer();
 
 function goalProgress() {
-    let max = document.getElementById("goal-numbers").innerText;
-    let current = document.getElementById("adopted-numbers").innerText;
-    let maxnum = parseFloat(max);
-    let currentnum = parseFloat(current);
-    let percentage = Math.floor((currentnum / maxnum) * 100);
-    let bar = document.getElementById("bar");
-    bar.style.height = `${percentage}` + "%";
-    let percentageLabel = document.getElementById("percentage");
-    percentageLabel.innerText = `${percentage}%`;
-    if (percentage === 100) {
-        percentageLabel.style.left = '25%'
-    }
+  let max = document.getElementById("goal-numbers").innerText;
+  let current = document.getElementById("adopted-numbers").innerText;
+  let maxnum = parseFloat(max);
+  let currentnum = parseFloat(current);
+  let percentage = Math.floor((currentnum / maxnum) * 100);
+  let bar = document.getElementById("bar");
+  bar.style.height = `${percentage}` + "%";
+  let percentageLabel = document.getElementById("percentage");
+  percentageLabel.innerText = `${percentage}%`;
+  if (percentage === 100) {
+    percentageLabel.style.left = "25%";
+  }
 }
 
 goalProgress();
